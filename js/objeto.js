7fecdc464c6f0d21735112afa56b2da5adfb3e09
ms-cris <personalmc@vivaldi.net>
2021-05-21 18:43:52 -0400

    Avance formulario y sección redes sociales

--- js/objeto.js
+++ js/objeto.js
@@ -1,32 +1,62 @@
-function button(){
-    alert('Hizo click en botón enviar');
-}
 
-/* function hacerclick(){
-    document.getElementsByTagName('input')[3].onclick = button;
-}
-
-window.onload = hacerclick; */
-
-var datos =[];
-function agregadatos(){
-    var datospersonales = new Object () ;
+function Validardatos(){
+    let datos = new Object () ;
     var nombre = document.getElementById('campo_nombre').value;
     var correo = document.getElementById('campo_correo').value;
     var telefono = document.getElementById('campo_telefono').value;
     var direccion = document.getElementById('campo_direccion').value;
-    var mensaje_text = document.getElementById('campo-mensaje').value;
+    var descripcion = document.getElementById('campo_mensaje').value;
 
 
-    datospersonales['campo_nombre'] = nombre ;
-    datospersonales['campo_correo'] =correo;
-    datospersonales['campo_telefono'] = telefono;
-    datospersonales['campo_direccion'] = direccion;
-    datospersonales['campo-mensaje'] = mensaje_text;
+    //Evaluamos nombre si está correcto 
 
+    if(nombre.length < 5){
+        document.getElementById("msgNombre").innerText = "El nombre debe de tener al menos 5 caracteres";
+    }else{
+        document.getElementById("msgNombre").innerText = "";
+        datos["nombre"] = nombre;
+    }
 
-    datos.push(datospersonales);
+    //Evaluamos correo si está correcto
 
-    console.log(datos);
+    if(correo.length > 0){
+        var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (regex.test(correo)){
+            document.getElementById("msgCorreo").innerText = "";
+        }else{
+            document.getElementById("msgCorreo").innerText = "Ingrese un correo válido.";
+            datos["correo"] = correo;
+        }
+        }else{
+            document.getElementById("msgCorreo").innerText = "El correo es obligatorio";
+        }
+
+        //EVALUO SI EL TELEFONO ES CORRECTO EN CHILE SON 9 DIGITOS
+        //ADEMÁS DEL NUMERO DE CARACTERES EVALUA QUE SEAN SOLO NUMEROS LOS QUE SE INGRESAN
+
+        if(telefono.length < 9){
+            document.getElementById("msgTel").innerText = "El telefono es invalido";
+        }else{
+            document.getElementById("msgTel").innerText = "";
+            datos["telefono"] = telefono;
+        }
+
+//EVALUAMOS QUE EL CAMPO DE DIRECCION TENGA MÁS DE 5 CARACTERES
+
+if(direccion.length < 5){
+    document.getElementById("msgText").innerText = "La dirección debe de tener al menos 5 caracteres";
+}else{
+    document.getElementById("msgText").innerText = "";
+    datos["direccion"] = direccion;
+}
+
+//Evaluo el campo de descripción
+if(descripcion.length <= 0){
+    document.getElementById("MsgDescripcion").innerText = "El mensaje es obligatorio";
+}else{
+    document.getElementById("MsgDescripcion").innerText = "";
+    datos["descripcion"] = descripcion;
+}
+console.log(datos);
 }
 
